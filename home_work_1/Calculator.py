32f2c85295f7b9b61dc171054a949be2f983b63c
Victor voloshchak <tanya.pomaranska@gmail.com>
2020-08-18 23:46:07 +0300

    Add first home work

---
+++ home_work_1/Calculator.py
@@ -0,0 +1,30 @@
+while True:
+    # Take input from the user
+    first_number = float(input("Enter first number: "))
+    operand=input("Enter operation with number:  +, -, *, /, **  ")
+    # Check if choice is one of the four options
+    if operand in ('+', '-', '*', '/','**'):
+
+        second_number = float(input("Enter second number: "))
+
+        if operand == '+':
+            print(first_number, "+", second_number, "=", first_number+second_number)
+
+        elif operand == '-':
+            print(first_number, "-", second_number, "=", first_number-second_number)
+
+        elif operand == '*':
+            print(first_number, "*", second_number, "=", first_number*second_number)
+
+        elif operand == '/':
+            if second_number==0 : print("You can't divide to 0")
+            else: print(first_number, "/", second_number, "=", first_number/second_number)
+
+        elif operand == '**':
+            print(first_number, "/", second_number, "=", first_number**second_number)
+        break
+    else:
+           if input("Invalid Input. Continue? y/n   ") == 'n':
+               break
+           else:
+               continue
