762eb9351a927ac690e0b5ec49319eaf8db76feb
Tanya Pomaranska <tanya.pomaranska@gmail.com>
2020-08-26 23:10:46 +0300

    Add file Task_1.py

---
+++ home_work_1/Task_1.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+place = input("Where do you live? ")
+
+print(f"Hello {name} \n\
+Your age is {age} \n\
+You live in  {place}")
