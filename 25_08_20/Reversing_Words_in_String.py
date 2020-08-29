1b2f6cc9cf5d81151032c60c893fdd6e9fa4bc4e
Tanya Pomaranska <tanya.pomaranska@gmail.com>
2020-08-29 22:40:25 +0300

    Add files

---
+++ 25_08_20/Reversing_Words_in_String.py
@@ -0,0 +1,8 @@
+#You need to write a function that reverses the words in a given string.
+# A word can also fit an empty string. If this is not clear enough, here are some examples:
+def reverse(st):
+    st = ' '.join(st.strip().split(' ')[::-1])
+    return st
+
+reverse('Hello World') == 'World Hello'
+reverse('Hi There.') == 'There. Hi'
