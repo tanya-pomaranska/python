5acc9e98e7fdf323ccebefc4f4117921d9e52f8e
Tanya Pomaranska <tanya.pomaranska@gmail.com>
2020-09-12 14:12:31 +0300

    remove string

--- Classwork/CW_4/NextVersion_pyowm.py
+++ Classwork/CW_4/NextVersion_pyowm.py
@@ -15,3 +15,12 @@ temperature = w.temperature('celsius')['temp']
 print("In " + city + " city" + " is the temperature of the air" + " " + str(temperature) + " for the Celsius")
 
 print("In this city is " + w.to_dict().get('detailed_status'))
+
+
+reg=owm.city_id_registry()
+list_of_locations=reg.locations_for('lviv')
+My_city= list_of_locations[0]
+print(f"For this coordinates{My_city.lat, My_city.lon} is city name: {My_city.name}")
+
+
+
